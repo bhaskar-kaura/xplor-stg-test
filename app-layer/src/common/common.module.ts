e11674423b1744b3d07abca28ed2a79208a15161
“abhishekd.nimbalkar” <abhishekd.nimbalkar@thewitslab.com>
2024-05-08 16:56:28 +0530

    feat: App layer set-up

---
+++ app-layer/src/common/common.module.ts
@@ -0,0 +1,13 @@
+import { Global, Module } from '@nestjs/common';
+import { AxiosService } from './axios/axios.service';
+import { HttpModule } from '@nestjs/axios';
+
+// Decorate the class with @Global() to make it a global module.
+@Global()
+@Module({
+  imports: [{ module: HttpModule, global: true }],
+  providers: [AxiosService],
+  exports: [AxiosService],
+})
+// Define the CommonModule class.
+export class CommonModule {}
--- app-layer/src/modules/app/app.module.ts
+++ app-layer/src/modules/app/app.module.ts
@@ -4,11 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from '../../config/env/env.config';
-import { JobModule } from '../job/job.module';
-import { ScholarshipModule } from '../scholarship/scholarship.module';
-import { CourseModule } from '../course/course.module';
-import { RetailModule } from '../retail/retail.module';
-import { CommonModule } from '../../common/common.module';
 
 @Module({
   imports: [
@@ -16,11 +11,6 @@ import { CommonModule } from '../../common/common.module';
       isGlobal: true,
       load: [configuration],
     }),
-    CommonModule,
-    CourseModule,
-    JobModule,
-    ScholarshipModule,
-    RetailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
