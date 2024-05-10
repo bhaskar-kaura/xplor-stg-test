c929e05a84dcab2e7c77bafe8a4670b0fd183528
Awnish <awnishk.singh@thewitslab.com>
2024-05-10 11:27:56 +0530

    feat: Implement on_search

--- app-layer/src/modules/app/app.service.ts
+++ app-layer/src/modules/app/app.service.ts
@@ -7,10 +7,19 @@ import { AckNackResponse } from 'src/common/action/ack-nack.entity';
 
 import { JobResponseService } from './response/job/job-response.service';
 import { DomainsEnum } from 'src/common/constants/enums';
+import { AxiosService } from 'src/common/axios/axios.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly globalActionService: GlobalActionService, private  createPayload:JobResponseService) {}
+  constructor(
+    private readonly globalActionService: GlobalActionService,
+    private createPayload: JobResponseService,
+    private readonly httpService: AxiosService,
+    private readonly configService: ConfigService
+  ) {
+    this.configService= new ConfigService();
+  }
   getHello(): string {
     return 'Hello World!';
   }
@@ -22,42 +31,53 @@ export class AppService {
         searchRequest.context,
         searchRequest.message,
       );
-      return  new AckNackResponse("ACK");
+      return new AckNackResponse('ACK');
     } catch (error) {
       console.log(JSON.stringify(error?.response?.data));
       throw error?.response;
     }
   }
   async onSearch(response: OnestContext | OndcContext | any) {
-   
-      await this.sendSearch(response)
-    
+    await this.sendSearch(response);
   }
-  
-  async sendSearch(response: SearchRequestDto)
-  {
-    let job:object|string="", course:object|string="", scholarship:object|string=""
-    switch (response.context.domain)
-    {
+
+  async sendSearch(response: SearchRequestDto) {
+    try {
+      let job: object | string = '',
+      course: object | string = '',
+      scholarship: object | string = '';
+    switch (response.context.domain) {
       case DomainsEnum.JOB_DOMAIN:
-        job = response.message ? this.createPayload.createPayload(response.message) : ""
+        job = response.message
+          ? this.createPayload.createPayload(response.message)
+          : '';
         break;
       case DomainsEnum.COURSE_DOMAIN:
-        course = response.message ? this.createPayload.createPayload(response.message) : ""
+        course = response.message
+          ? this.createPayload.createPayload(response.message)
+          : '';
         break;
       case DomainsEnum.SCHOLARSHIP_DOMAIN:
-        scholarship = response.message ? this.createPayload.createPayload(response.message) : ""
+        scholarship = response.message
+          ? this.createPayload.createPayload(response.message)
+          : '';
         break;
       default:
-        break
-      
+        break;
     }
     const payload = {
       job: job,
       course: course,
-      scholarship:scholarship
+      scholarship: scholarship,
+    };
+      const url = (this.configService.get('CORE_SERVICE_URL') + '/stg/on_search')
+      console.log("url",url)
+      const resp = await this.httpService.post(url, payload);
+      console.log('resp', resp)
+    } catch (error) {
+      console.log(error?.message)
+      return error?.message
     }
-    console.log(JSON.stringify(payload.course))
+
   }
-  
 }
